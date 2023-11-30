8d6c1e57d46e4465d95c1eea357237cbdc0ebc04
DeAndre Boston <dboston3@yahoo.com>
2023-11-29 20:26:51 -0600

    add a comma

--- app/components/pricetable.tsx
+++ app/components/pricetable.tsx
@@ -48,7 +48,7 @@ const pricetable = () => {
               <h3 className="mb-4 text-2xl font-semibold">Package 2</h3>
               <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">Best value.</p>
               <div className="flex justify-center items-baseline my-8">
-                  <span className="mr-2 text-5xl font-extrabold">$2500</span>
+                  <span className="mr-2 text-5xl font-extrabold">$2,500</span>
               </div>
               {/* <!-- List --> */}
               <ul role="list" className="mb-8 space-y-4 text-left">
