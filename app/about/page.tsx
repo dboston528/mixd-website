f2d3b0a8b9d8e382ade56baa781ba083c0983b55
DeAndre Boston <dboston3@yahoo.com>
2024-01-23 22:11:27 -0600

    update about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,9 +8,7 @@ export default function Page() {
     return (
       <div>
         <Navbar></Navbar>
-        <h1 className="pt-5 text-center mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Pricing</h1>
-
-
+        <h1 className="pt-5 text-center mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">About</h1>
         
         <Footer></Footer>
       </div>
