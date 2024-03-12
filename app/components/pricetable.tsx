ab0441b385e21f4e224d7ad3d2106f982c399250
DeAndre Boston <dboston3@yahoo.com>
2024-03-12 14:33:33 -0500

    correct spelling

--- app/components/pricetable.tsx
+++ app/components/pricetable.tsx
@@ -4,7 +4,7 @@ const pricetable = () => {
     
   <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
       <div className="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
-          <p className="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">New pricing packages comming soon...</p>
+          <p className="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">New pricing packages coming soon...</p>
           <a href="https://www.honeybook.com/widget/mixd_chicago_271581/cf_id/65c528b2b9e21200253deb54" className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-teal-600 rounded-lg hover:bg-teal-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
     Contact Us
     <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
