1b4952e5fa1ee4e6955b264cccaac073173289c5
DeAndre Boston <deandreboston@DeAndres-MacBook-Pro.local>
2025-11-24 21:58:09 -0600

    update spotify config

---
+++ types/spotify-web-api-node.d.ts
@@ -0,0 +1,21 @@
+declare module 'spotify-web-api-node' {
+  interface SpotifyWebApiConfig {
+    clientId?: string;
+    clientSecret?: string;
+    redirectUri?: string;
+    refreshToken?: string;
+  }
+
+  class SpotifyWebApi {
+    constructor(config?: SpotifyWebApiConfig);
+    setAccessToken(token: string): void;
+    setRefreshToken(token: string): void;
+    authorizationCodeGrant(code: string): Promise<any>;
+    refreshAccessToken(): Promise<any>;
+    getUserPlaylists(): Promise<any>;
+    getPlaylist(id: string): Promise<any>;
+    getPlaylistTracks(id: string, options?: Record<string, any>): Promise<any>;
+  }
+
+  export default SpotifyWebApi;
+}
