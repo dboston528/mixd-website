441743e8a9b5cdc8fb67e46f5f717b86a95374b5
DeAndre Boston <deandreboston@DeAndres-MacBook-Pro.local>
2026-01-05 17:48:15 -0600

    add brochure

---
+++ app/brochure/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+import { Card } from "flowbite-react";
+
+export default function Page() {
+  return (
+    <div className="bg-white">
+      <Navbar></Navbar>
+      <div style={{position: 'relative', width: '100%', height: '0', paddingTop: '77.2727%', paddingBottom: '0', boxShadow: '0 2px 8px 0 rgba(63,69,81,0.16)', marginTop: '1.6em', marginBottom: '0.9em', overflow: 'hidden', borderRadius: '8px', willChange: 'transform', borderRadius: '8px'}}>
+  <iframe loading="lazy" style={{position: 'absolute', width: '100%', height: '100%', top: '0', left: '0', border: 'none', padding: '0', margin: '0'}}
+    src="https://www.canva.com/design/DAGULBybMW8/t7GksNVjSxiw4qitjVhV9w/view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+  </iframe>
+</div>
+<a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGULBybMW8&#x2F;t7GksNVjSxiw4qitjVhV9w&#x2F;view?utm_content=DAGULBybMW8&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">Wedding Entertainment Brochure</a> by Brad Jones
+      <Footer></Footer>
+    </div>
+  );
+}
